af68a665140638b1ebfe05d02ba93f72c65f6bb2
educalleja <e.calleja.garcia@gmail.com>
2019-07-25 13:09:10 +0200

    Refactors tests for entrypoint.js

--- src/__tests__/entrypoint.spec.js
+++ src/__tests__/entrypoint.spec.js
@@ -3,26 +3,18 @@ import { toggleyApi } from '../featureProviders';
 
 jest.mock('../featureProviders');
 
-test('Returns true when feature is set to True', async () => {
-    toggleyApi.getTreatmentsFromService = jest.fn().mockReturnValue({
-        'feature1234': true
-    })
-    const result = await isEnabled('feature1234', 'userId1');
-    expect(result).toBeTruthy();
+test.each([true, false])('Returns %s when feature is set to that value', async (valueFeature) => {
+  toggleyApi.getTreatmentsFromService = jest.fn().mockReturnValue({
+    feature1234: valueFeature,
+  });
+  const result = await isEnabled('feature1234', 'userId1');
+  expect(result).toBe(valueFeature);
 });
 
-test('Returns false when feature is set to False', async () => {
-    toggleyApi.getTreatmentsFromService = jest.fn().mockReturnValue({
-        'feature1234': false
-    })
-    const result = await isEnabled('feature1234', 'userId1');
-    expect(result).toBeFalsy();
-});
-
-test('Returns false when feature is not set for this user', async () => {
-    toggleyApi.getTreatmentsFromService = jest.fn().mockReturnValue({
-        'feature1234': false
-    })
-    const result = await isEnabled('feature1234', 'userId1');
-    expect(result).toBeFalsy();
+test('Returns false when feature is not set for a user', async () => {
+  toggleyApi.getTreatmentsFromService = jest.fn().mockReturnValue({
+    feature1234: false,
+  });
+  const result = await isEnabled('feature1234', 'userId1');
+  expect(result).toBeFalsy();
 });

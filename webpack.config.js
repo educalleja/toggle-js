326fb865ef3261465e19e7442857d491535136b2
educalleja <e.calleja.garcia@gmail.com>
2019-07-26 15:15:46 +0200

    Rollback source-map for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = [
-    {
-        mode: "development",
-        devtool: "cheap-source-map",
-        path: `${path.resolve(__dirname, 'dist')}/dev`
-    },
-    {
-        mode: "production",
-        devtool: false,
-        path: `${path.resolve(__dirname, 'dist')}/prod`
-    },
-].map(({mode, devtool, path}) => ({
-    mode,
-    devtool,
-    entry: './src/main.js',
-    output: {
-      path,
-      filename: 'toggley.js'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, use: 'babel-loader' },
-      ]
-    }
+  {
+    mode: "development",
+    devtool: "source-map",
+    path: `${path.resolve(__dirname, 'dist')}/dev`
+  },
+  {
+    mode: "production",
+    devtool: false,
+    path: `${path.resolve(__dirname, 'dist')}/prod`
+  },
+].map(({ mode, devtool, path }) => ({
+  mode,
+  devtool,
+  entry: './src/main.js',
+  output: {
+    path,
+    filename: 'toggley.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+    ]
+  }
 }));

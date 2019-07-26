a9f86e07b3b94783fd1d497549ebedfbe43fb3ca
educalleja <e.calleja.garcia@gmail.com>
2019-07-26 16:05:37 +0200

    Fixes linting in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
 module.exports = function (api) {
-  const targets = api.env('test') ? { node: 'current' } : '> 0.25%, not dead';
+  const targets = api.env('test') ? { node: 'current' } : { chrome: 60 };
   api.cache(true);
   const presets = [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         corejs: 3,
-        useBuiltIns: "entry",
-        targets
-      }
-    ]
+        useBuiltIns: 'entry',
+        targets,
+      },
+    ],
   ];
 
   return { presets };

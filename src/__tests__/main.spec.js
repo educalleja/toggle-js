5a06ec2c9d716947e0f02c85bff9b6cda3c07460
educalleja <e.calleja.garcia@gmail.com>
2019-07-28 13:14:27 +0200

    APITOKEN must be set before calling isEnabled

--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -1,12 +1,21 @@
-import { isEnabled } from '../main';
+import { init, isEnabled } from '../main';
 import provider from '../featureProviders';
 
 jest.mock('../featureProviders');
+
+beforeEach(() => {
+  init(undefined);
+});
+
+test('throws exception when token is not provided', async () => {
+  expect(isEnabled()).rejects.toEqual(new Error('Toggley - API KEY not set. Call init function first'));
+});
 
 test.each([true, false])('Returns %s when feature is set to that value', async (valueFeature) => {
   provider.mockImplementation(() => ({
     getTreatmentsFromService: () => ({ feature1234: valueFeature }),
   }));
+  init('TESTAPIKEY');
   const result = await isEnabled('feature1234', 'userId1');
   expect(result).toBe(valueFeature);
 });
@@ -15,6 +24,7 @@ test('Returns false when feature is not set for a user', async () => {
   provider.mockImplementation(() => ({
     getTreatmentsFromService: () => ({}),
   }));
+  init('TESTAPIKEY');
   const result = await isEnabled('feature1234', 'userId1');
   expect(result).toBeFalsy();
 });
--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import store from './store';
 import treatments from './treatments';
 import provider from './featureProviders';
 
+let APIKEY;
+
+export function init(token) {
+  APIKEY = token;
+}
+
 export const isEnabled = async (featureName, userId) => {
+  if (!APIKEY) {
+    throw new Error('Toggley - API KEY not set. Call init function first');
+  }
   const treats = treatments(provider(), store());
   return treats.isEnabledForUser(featureName, userId);
 };

6532632722e122e3fbd281b11e91285ada77c41b
educalleja <e.calleja.garcia@gmail.com>
2019-07-28 13:20:25 +0200

    Changes arrow function to named function

--- src/main.js
+++ src/main.js
@@ -4,19 +4,27 @@ import store from './store';
 import treatments from './treatments';
 import provider from './featureProviders';
 
+// import { init, isEnabled } from 'Toggley'
+// init('ZZ');
+// isEnabled('a', 'b');
+
+
+// cdn
+// script src="blabla.com/bundle.js?token=XXXXXX"
+// window.Toogley.isEnabled('a', 'b');
 let APIKEY;
 
 export function init(token) {
   APIKEY = token;
 }
 
-export const isEnabled = async (featureName, userId) => {
+export async function isEnabled(featureName, userId) {
   if (!APIKEY) {
     throw new Error('Toggley - API KEY not set. Call init function first');
   }
   const treats = treatments(provider(), store());
   return treats.isEnabledForUser(featureName, userId);
-};
+}
 
 if (typeof window !== 'undefined') {
   window.Toggley = { isEnabled };
